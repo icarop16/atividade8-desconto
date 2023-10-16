b9e8e9baf85c85c82ce07dfcb4dc7d3ea91565f2
icarop16 <pedroicaro161107@gmail.com>
2023-10-16 15:22:10 -0300

    Atividade Desconto

---
+++ pedroicaro.py
@@ -0,0 +1,9 @@
+# Informe um valor e veja quanto de desconto você terá em R$, receba a % do desconto que quer receber;
+valor = float(input("Digite um valor em real: "))
+desconto = float(input("Digite a '%' de desconto: "))
+desconto = (valor * desconto)/100
+resultado = valor-desconto
+print(f"""
+        Valor de Desconto: {desconto}
+        Valor Final: {resultado} 
+""")
